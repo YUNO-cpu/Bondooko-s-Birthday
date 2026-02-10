60c33f617066e39230ea6ac1cef35c9cc388d0e1
YUNO-cpu <flamesixtyfire@outlook.com>
2026-02-10 13:41:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let currentPage = 0;
+const pages = document.querySelectorAll(".letter-page");
+
 function openLetter() {
-  document.getElementById("letter").classList.toggle("open");
+  document.getElementById("letter").classList.add("open");
 }
+
+function nextPage() {
+  pages[currentPage].classList.remove("active");
+  currentPage++;
+
+  if (currentPage < pages.length) {
+    pages[currentPage].classList.add("active");
+  } else {
+    currentPage = pages.length - 1;
+  }
+}
+
