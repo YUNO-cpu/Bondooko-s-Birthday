e6a3940961aff7cd42246b96dd1ecc76bdf6d61d
YUNO-cpu <flamesixtyfire@outlook.com>
2026-02-10 15:52:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+const gift = document.getElementById("gift");
+const letter = document.getElementById("letter");
+const pages = document.querySelectorAll(".page");
 let currentPage = 0;
-const pages = document.querySelectorAll(".letter-page");
 
+// 4 секундийн дараа зураг гарч ирнэ
+setTimeout(() => {
+  gift.classList.add("show");
+}, 4000);
+
+// Зураг дээр дарахад захиа гарна
 function openLetter() {
-  document.getElementById("letter").classList.add("open");
+  letter.style.display = "block";
+  gift.style.display = "none";
 }
 
+// Next button
 function nextPage() {
   pages[currentPage].classList.remove("active");
   currentPage++;
 
   if (currentPage < pages.length) {
     pages[currentPage].classList.add("active");
-  } else {
-    currentPage = pages.length - 1;
   }
 }
-
